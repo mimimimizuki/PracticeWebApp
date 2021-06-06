950d140515552e1ad81bdfc8778a1fa450c7242b
Yeongse <74057052+Yeongse@users.noreply.github.com>
2021-06-06 21:55:01 +0900

    Dockerの環境構築に挑戦中

--- backend/api.py
+++ backend/api.py
@@ -1,2 +1,23 @@
 # ここにコード書いてね
-print("岡部瑞稀")
+from flask import Flask,jsonify,request
+import json
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def hello():
+  return "Hello World!"
+
+@app.route("/api", methods=['POST'])
+def reply():
+  data = json.loads(request.data)
+  answer = "Yes,it is %s! \n" %data["keyword"]
+  result ={
+    "Content-Type":"application/json",
+    "Answer":{"Text":answer}
+  }
+  #return answer
+  return jsonify(result)
+
+  if __name__ == "__main__":
+    app.run(host = '0.0.0.0', port=5000,debug=True)
+    
