0cc919ad673006ae893112024b5b155d0b4b843f
Yeongse <74057052+Yeongse@users.noreply.github.com>
2021-06-13 23:58:41 +0900

    簡単な表示の実行。次はPOSTに挑戦

--- backend/api.py
+++ backend/api.py
@@ -1,23 +1,30 @@
-# ここにコード書いてね
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify,request,render_template
 import json
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.types import Integer
+from sqlalchemy.types import String
+from sqlalchemy.types import DateTime
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.intern"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-@app.route("/", methods=['GET'])
-def hello():
-  return "Hello World!"
+db = SQLAlchemy(app)
 
-@app.route("/reply", methods=['POST'])
-def reply():
-  data = json.loads(request.data)
-  answer = "Yes,it is %s! \n" %data["keyword"]
-  result ={
-    "Content-Type":"application/json",
-    "Answer":{"Text":answer}
-  }
-  #return answer
-  return jsonify(result)
+class Intern(db.Model):
+  id = db.Column(db.Integer,primary_key=True)
+  company_name = db.Column(db.String(128),nullable=False)
+  skills = db.Column(db.String(128),nullable=False)
+  start = db.Column(db.DateTime,nullable=False)
+  end = db.Column(db.DateTime,nullable=False)
 
-  if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port=5000,debug=True)
-    
+@app.route("/")
+def index():
+  return render_template("index.html",title = "change test")
+
+
+
+if __name__ == "__main__":
+  #db.create_all()
+  app.run(host="0.0.0.0",port = 5000, debug =True)
