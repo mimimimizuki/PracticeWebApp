b167fe0ddf650b6636cbbaa8f07c659d3ffe407b
admin <admin@h93.187.229.10.1017421.vlan.kuins.net>
2021-06-21 16:34:00 +0900

    connect with backend

--- backend/api.py
+++ backend/api.py
@@ -1,1 +1,35 @@
-# ここにコード書いてね
+from flask import Flask,jsonify,request,render_template
+import json
+from flask_cors import CORS
+
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy.types import Integer
+# from sqlalchemy.types import String
+# from sqlalchemy.types import DateTime
+
+app = Flask(__name__)
+CORS(app)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.intern"
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# db = SQLAlchemy(app)
+
+# class Intern(db.Model):
+#   id = db.Column(db.Integer,primary_key=True)
+#   company_name = db.Column(db.String(128),nullable=False)
+#   skills = db.Column(db.String(128),nullable=False)
+#   start = db.Column(db.DateTime,nullable=False)
+#   end = db.Column(db.DateTime,nullable=False)
+
+@app.route("/intern")
+def index():
+  return {"companies":["楽天株式会社", "LINE株式会社", "株式会社VOYAGE", "メルカリ"],
+          "siteURLs":["","","https://voyagegroup.snar.jp/",""],
+          "internURLs":["","","","https://mercan.mercari.com/articles/28040/"]}
+  # return render_template("index.html",title = "change test")
+
+
+
+if __name__ == "__main__":
+  #db.create_all()
+  app.run(host="0.0.0.0",port = 5000, debug =True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,44 @@
 import './App.css';
 import logo from "./logo.svg";
 import axios from 'axios';
+import React from "react"
 
-function App() {
-  axios.get("http://localhost:5000").then(res => {
-    console.log(res);
-  }).catch(err => {
-    console.log(err)
-  })
-  const companies = ["楽天株式会社", "LINE株式会社", "株式会社VOYAGE", "メルカリ"];
-  const siteURLs = ["", "", "https://voyagegroup.snar.jp/", ""]
-  const internURLs = ["","","","https://mercan.mercari.com/articles/28040/"]
-  const showCompany = companies.map((element, i) => 
-  <div className="cardWrapper">
-  <div className="card" key={i}>
-    <img class="card-img" src={logo} alt=""></img>
-      <div className="card-content">
-        <h1 className="card-title">{element}</h1>
-      </div>
-    <div className="card-link">
-    <a href={siteURLs[i]}>About</a>
-    <a href={internURLs[i]}>Website</a>
-    </div>
-  </div>
-  </div>
-)
+export default class Layout extends React.Component{
+  constructor(props) {
+    super();
+    this.state = {companies : [], siteURLs:[], internURLs:[]}
+  }
+  componentDidMount(e){
+    axios.get("http://localhost:5000/intern").then(res => {
+      this.setState({"companies":res.data["companies"], "siteURLs":res.data["siteURLs"], "internURLs":res.data["internURLs"]})
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+render() {
   return (
     <div className="App">
       <header className="App-header">
         <h1> Internship infomation center </h1>
       </header>
       <div>
-        {showCompany}
+          {this.state.companies.map((element, i) => 
+          <div className="cardWrapper">
+          <div className="card" key={i}>
+            <img class="card-img" src={logo} alt=""></img>
+              <div className="card-content">
+                <h1 className="card-title">{element}</h1>
+              </div>
+            <div className="card-link">
+            <a href={this.state.siteURLs[i]}>About</a>
+            <a href={this.state.internURLs[i]}>Website</a>
+            </div>
+          </div>
+          </div>
+        )}
       </div>
     </div>
   );
 }
-
-export default App;
+}
